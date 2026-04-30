54046e8198d8155989716db45e4e7ef5cf3e30f3
Galih Pangestu <pangestu5711@gmail.com>
2026-05-01 04:31:46 +0700

    fix: disable stateful middleware to prevent csrf mismatch

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,7 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \App\Http\Middleware\LogActivity::class,
         ]);
 
